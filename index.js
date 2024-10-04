b4804008dd856289ecc51a46f0ade15fafac6ddc
Saif <saifansari20576@gmail.com>
2024-10-05 04:57:25 +0530

    Initial commit: Add index.js with prime number finder

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function findPrimeNumbers(arr) {
+    const primeNumbers = [];
+    arr.forEach(num => {
+        if (isPrime(num)) {
+            primeNumbers.push(num);
+        }
+    });
+    return primeNumbers;
+}
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const primeNumbers = findPrimeNumbers(numbers);
+
+console.log("Prime Numbers:", primeNumbers);
